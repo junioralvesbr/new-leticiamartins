34d55c33a10421ab7f2875cb82600798f82df16d
Junior Alves <jrnalves@gmail.com>
2023-09-28 10:56:29 -0300

    create Card section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import Cards from "@/components/Cards";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <>
+    <div className="sm:px-10 xl:px-32">
       <header className="pt-5 md:pt-10">
         <Header />
       </header>
-    </>
+      <main>
+        <Cards />
+      </main>
+    </div>
   )
 }
