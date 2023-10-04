a025ca6236fece5da237eac7c0352ab426fa4a4c
Junior Alves <jrnalves@gmail.com>
2023-10-04 17:56:10 -0300

    metaData config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { Metadata } from "next";
 import Header from "@/components/Header";
 import Cards from "@/components/Cards";
 import Services from "@/components/Services";
 import Aboult from "@/components/About";
 import Video from "@/components/Video";
 import Course from "@/components/Course";
+
+
+export const metadata: Metadata = {
+  title: 'Leticia Martins Personal Organizer',
+}
 
 export default function Home() {
   return (
