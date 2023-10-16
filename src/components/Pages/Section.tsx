5401982c759df87b14ca6940cbbad0d384235bf2
Junior Alves <jrnalves@gmail.com>
2023-10-16 14:00:05 -0300

    refactory

---
+++ src/components/Pages/Section.tsx
@@ -0,0 +1,46 @@
+import { WhatsAppButton } from '../Whatsapp'
+
+interface SectionProps {
+  data: {
+    image: string,
+    alt: string,
+    title: string,
+    text: string,
+    reverse: boolean
+  }
+}
+
+export default function Section({ data }: SectionProps) {
+  return (
+    <section>
+      {data && data.map((item, index) => (
+        <article
+          className={`flex flex-col justify-center items-center gap-5 mb-24 md:gap-8
+            ${item.reverse ? "md:flex-row-reverse" : "md:flex-row"}
+            `}
+          key={index}
+        >
+
+          <figure className="lg:max-w-[640px]">
+            <img src={item.image} alt="foto de closet"
+              className="w-full h-auto"
+            />
+          </figure>
+
+          <div className="lg:max-w-[640px]">
+
+            <h2 className="font-bold text-center text-seventh-color uppercase tracking-widest pb-5">
+              {item.title}
+            </h2>
+
+            <p className="text-center text-secondary-color px-4 lg:text-lg min-w-[320px] max-w-[600px]">
+              {item.text}
+            </p>
+          </div>
+        </article>
+      ))}
+
+      <WhatsAppButton>Entre em contato comigo</WhatsAppButton>
+    </section>
+  )
+}
